59155b20ab700df62f70d14f0a8ce94cd69037d5
Zack-Z23 <zack.zachairidis@gmail.com>
2026-05-01 19:11:31 -0500

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
   char command[1024];
-  fget(command, sizeof(command), stdin);
-  command[strcspn(command, "\n")] = '\0';
-  if(command != " "){
-    printf("%s: command not found", command);
-  }
+
   printf("$ ");
+
+  fgets(command, sizeof(command), stdin);
+  command[strcspn(command, "\n")] = '\0';
+  printf("%s: command not found", command);
 
   return 0;
 }
