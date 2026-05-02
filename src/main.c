674180b80ab3b339217fc4e67dec2557ee34dd5a
Zack-Z23 <zack.zachairidis@gmail.com>
2026-05-01 19:27:30 -0500

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[]) {
   setbuf(stdout, NULL);
   char command[1024];
   while(1){
-  printf("$");
+  printf("$ ");
 
   fgets(command, sizeof(command), stdin);
   command[strcspn(command, "\n")] = '\0';
