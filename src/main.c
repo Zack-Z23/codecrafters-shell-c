401cdb357e3448419dcf635e5ef10fad2457f7e3
Zack-Z23 <zack.zachairidis@gmail.com>
2026-05-01 19:08:24 -0500

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -4,8 +4,12 @@
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
-
-
+  char command[1024];
+  fget(command, sizeof(command), stdin);
+  command[strcspn(command, "\n")] = '\0';
+  if(command != " "){
+    printf("%s: command not found", command)
+  }
   printf("$ ");
 
   return 0;
